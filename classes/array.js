132e1596e1682451ad0ab060d00fe6a625956e4e
Alok Suman <71497735+Alokray007@users.noreply.github.com>
2023-10-19 00:44:34 +0530

    1st commit

---
+++ classes/array.js
@@ -0,0 +1,13 @@
+Array.prototype.isEqual = function(array) {
+    if (this.length !== array.length) {
+        return false
+    }
+    for (let i=0; i< this.length; i++) {
+        if (this[i] !== array[i]) {
+            return false
+        }
+    }
+    return true;
+
+}
+
